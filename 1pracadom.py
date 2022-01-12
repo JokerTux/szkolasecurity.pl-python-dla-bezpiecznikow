5fe20f8aa36c60e633a17ba01be922763214a148
Patrick <49305052+JokerTux@users.noreply.github.com>
2022-01-12 06:40:22 +0100

    Update 1pracadom.py

--- 1pracadom.py
+++ 1pracadom.py
@@ -1,3 +1,13 @@
+'''
+Praca domowa
+
+Stwórz skrypt, który:
+1. Tworzy listę liczb od 0 do 9 (pętla for)
+2. Iteruje po tej liście pętlą for lub while i wypisuje jej zawartość
+3. Usuwa liczby 3 i 5 z listy [ list.remove() ]
+3. Zamienia liczby 8 i 9 na stringi (list.remove() lub list.pop() i list.append(), bądź zwykłe przypisanie list[8] = '8′
+Lista po tych przekształceniach powinna wyglądać tak: [0, 1, 2, 4, 6, 7, '8′, '9′]
+'''
 
 lista = []
 
