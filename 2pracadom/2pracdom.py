e0289e23299252888ead9d00811708209271c83d
Patrick <49305052+JokerTux@users.noreply.github.com>
2022-01-17 21:52:38 +0100

    Add files via upload

---
+++ 2pracadom/2pracdom.py
@@ -0,0 +1,45 @@
+import re
+
+if __name__ == '__main__':
+	def PhpFilter():	
+		# Open 'fuff.log' and read the file
+		with open('ffuf.log', 'r') as infile:		
+			logs = infile.read()
+			# Splits for a better view
+			for log in logs.split('\n'):
+				log = log.replace(',', ' ')
+				log_split = log.split(' ')
+				
+				path = log_split[0]
+				status_code = log_split[2]
+				size = log_split[5]
+				
+				#print(f'{path}|{status_code}|{size}')
+				user = re.search('user.php$', path)
+				admin = re.search('admin.php$', path)
+
+				# Whats a better way to write this 4 lines ?
+				if user:
+					print(path)
+				if admin:
+					print(path)	
+
+				
+
+		
+		
+	PhpFilter()			
+
+
+
+
+
+
+
+
+
+	
+		
+
+		
+			
